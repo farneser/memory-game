1a0a4bfbadf407e6f4ad952c5cc70dd5ee99f705
farneser <farneser1@gmail.com>
2023-10-11 19:37:07 +0300

    feat: card container component

---
+++ components/CardContainer.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+interface CardContainerProps {
+    children: React.ReactNode;
+}
+
+const CardContainer: React.FC<CardContainerProps> = ({ children }) => {
+    return (
+        <View style={styles.container}>
+            {children}
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+    },
+});
+
+export default CardContainer;
